dbfccc7bfe94df420937bc60e374a729487438c9
huawenbo <86921617+huawenbo@users.noreply.github.com>
2021-07-16 11:49:23 +0800

    Add files via upload

---
+++ model_development/LR.py
@@ -0,0 +1,7 @@
+def LR_model(x_train, y_train, x_test, y_test):
+    model = LogisticRegression()  
+    model.fit(x_train,y_train)
+    pred_train = model.predict_proba(x_train)[:,-1]
+    pred_test = model.predict_proba(x_test)[:,-1]
+    result = [roc_auc_score(y_train,pred_train), roc_auc_score(y_test,pred_test)]
+    return model, result, (y_test.values, pred_test)
